ed6ce5ae1e2b4f9ff95425f80bcdcd18f19c0e0f
HeraZhej <tuguinayjeborah@yahoo.com>
2023-08-24 21:37:32 +0800

    Dashboard done

--- psytal/vite/tailwind.config.js
+++ psytal/vite/tailwind.config.js
@@ -14,6 +14,11 @@ export default {
               'viridianHue' : '#496C54',
               'neonGreen' : '#35B736',
               'highlightGreen' : '#B5E3B4',
+              'grayGreen' : '#DBEADC',
+              'dash1' : '#1F9563',
+              'dash2' : '#1F9586',
+              'dash3' : '#20951F',
+              'dash4' : '#6B951F',
       }
     },
   },
