cea7655cd72b930d9a87a6824ae62d7d459c11e4
JanDwight <monreal.jandwight@gmail.com>
2023-09-03 22:26:54 +0800

    Menu

--- psytal/vite/src/views/authorized_view/views_main/Classes.jsx
+++ psytal/vite/src/views/authorized_view/views_main/Classes.jsx
@@ -8,13 +8,13 @@ export default function Classes(){
 
     return(
     <>
-        <div className="w-full h-[500px] px-4 mx-auto  rounded-3xl bg-white shadow-2xl pt-5 pb-12">{/*For the Container*/}
+        <div className="w-full h-[500px] rounded-3xl bg-white shadow-2xl pt-5 pb-12">{/*For the Container*/}
             <div className="mt-5 mx-5 pb-5 border-b-2 border-black flex flex-row justify-between items-baseline">
-                <div className="font-bold text-6xl text-[#525252]">Classess</div>
+                <div className="font-bold text-4xl lg:text-6xl text-[#525252]">Classes</div>
 
                 {/*Filter and Add classes */}
                 <div className="flex flex-row">
-                    <div>
+                    <div className="hidden md:hidden lg:contents">
                         <Menu>
                             <Menu.Button className="relative mr-5 py-1 px-5 border-2 border-black">Filter</Menu.Button>
                             <Menu.Items className="absolute px-[7px] border-2 border-black bg-white">
@@ -69,34 +69,82 @@ export default function Classes(){
             </div>
 
             <div className="m-5">
+                <div className=" md:hidden lg:hidden">
+                    <Menu>
+                        <Menu.Button className="relative mr-5 py-1 px-5 border-2 border-black">Filter</Menu.Button>
+                        <Menu.Items className="absolute px-[7px] border-2 border-black bg-white">
+                            <div className="flex flex-col">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <a
+                                        className={`${active && 'bg-[#CCEFCC]'} border-b-2 border-black`}
+                                    >
+                                      1st Year
+                                    </a>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  className={`${active && 'bg-[#CCEFCC]'} border-b-2 border-black`}
+                                >
+                                  2nd Year
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  className={`${active && 'bg-[#CCEFCC]'} border-b-2 border-black`}
+                                  href="/account-settings"
+                                >
+                                  3rd Year
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
+                                  className={`${active && 'bg-[#CCEFCC]'}`}
+                                  href="/account-settings"
+                                >
+                                  4th Year
+                                </a>
+                              )}
+                            </Menu.Item>
+                            </div>
+                        </Menu.Items>
+                    </Menu>
+                </div>
+
                 <div className="mx-7 flex flex-col-4 justify-between">
-                    <div>Course Code</div>
+                    <div className="hidden md:hidden lg:contents">Course Code</div>
                     <div>Description</div>
                     <div>Year/Section</div>
-                    <div>Instructor</div>
+                    <div className="hidden md:hidden lg:contents">Instructor</div>
                 </div>
 
                 <div className="mt-2">
                     <a href="https://classroom.google.com/" target="_blank" rel="noopener noreferrer"
                        className="bg-[#7EBA7E] rounded-full flex justify-between py-2 px-5 m-2 shadow-2xl">
-                            <div>Course Code</div>
+                            <div className="hidden md:hidden lg:contents">Course Code</div>
                             <div>Description</div>
                             <div>Year/Section</div>
-                            <div>Instructor</div>
+                            <div className="hidden md:hidden lg:contents">Instructor</div>
                     </a>
                     <a href="https://classroom.google.com/" target="_blank" rel="noopener noreferrer"
                        className="bg-[#7EBA7E] rounded-full flex justify-between py-2 px-5 m-2 shadow-2xl">
-                            <div>Course Code</div>
+                            <div className="hidden md:hidden lg:contents">Course Code</div>
                             <div>Description</div>
                             <div>Year/Section</div>
-                            <div>Instructor</div>
+                            <div className="hidden md:hidden lg:contents">Instructor</div>
                     </a>
                     <a href="https://classroom.google.com/" target="_blank" rel="noopener noreferrer"
                        className="bg-[#7EBA7E] rounded-full flex justify-between py-2 px-5 m-2 shadow-2xl">
-                            <div>Course Code</div>
+                            <div className="hidden md:hidden lg:contents">Course Code</div>
                             <div>Description</div>
                             <div>Year/Section</div>
-                            <div>Instructor</div>
+                            <div className="hidden md:hidden lg:contents">Instructor</div>
                     </a>
                 </div>
             </div>
