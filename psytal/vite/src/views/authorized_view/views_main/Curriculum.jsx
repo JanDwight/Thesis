d2e530aedab69794de5c8b22abfec57fec811539
Hilaria14 <lawaganhilaria@gmail.com>
2023-09-06 13:49:28 +0800

    updates

--- psytal/vite/src/views/authorized_view/views_main/Curriculum.jsx
+++ psytal/vite/src/views/authorized_view/views_main/Curriculum.jsx
@@ -2,7 +2,24 @@ import React from "react";
 
 export default function Curriculum(){
     return(
-        <div>curriculum</div>
+
+        <div className="w-full px-4 mx-auto mt-0">
+            {/**______________________BOX___________________________ */}
+            <div className='block rounded-3xl bg-white text-center drop-shadow-xl'>
+                {/**__________________Curriculum & Button_____________ */}
+                <div className="text-left pt-2 px-8">
+                    <h1 className='text-3xl font-bold my-5 text-zinc-600'>CURRICULUM</h1>
+                    <hr className="border-gray-400 mt-2 mb-2"/>
+                </div>
+                {/**__________________1st Year________________________ */}
+                <div className="text-center">
+                    <h1 className='text-3xl font-semibold my-5 text-neutral-800 mt-5'>1st Year</h1>
+                </div>
+
+                
+                
+            </div>
+        </div>
 
 
     )}
--- psytal/vite/src/views/authorized_view/views_main/ManageUsers.jsx
+++ psytal/vite/src/views/authorized_view/views_main/ManageUsers.jsx
@@ -64,51 +64,51 @@ export default function ManageUsers() {
 
   return (
     <div className="w-full px-4 mx-auto mt-0">
-    <div className="rounded-t bg-white h-10 px-6 pt-5 pb-12">
-    <div className="flex justify-between">
-    <h6 className="block uppercase tracking-wide text-green-700 text-base font-semibold">manage user accounts</h6>
+      <div className="rounded-t bg-white h-10 px-6 pt-5 pb-12">
+        <div className="flex justify-between">
+          <h6 className="block uppercase tracking-wide text-green-700 text-base font-semibold">manage user accounts</h6>
     
-    <button
-        onClick={() => setShowModal(true)}
-        className="bg-lime-600 hover:bg-lime-700 text-white text-sm font-semibold py-1 px-4 "
+          <button
+            onClick={() => setShowModal(true)}
+            className="bg-lime-600 hover:bg-lime-700 text-white text-sm font-semibold py-1 px-4 "
+            
+          >
+            Add User
         
-      >
-        Add User
-        
-      </button>
-      </div>
-      </div>
-      <ul className="flex mb-0 list-none flex-wrap px-4 flex-row text-gray-700">
-        <Tab
-          label="students list"
-          isActive={activeTab === 1}
-          onClick={() => setActiveTab(1)}
-        />
-        <Tab
-          label="employees list"
-          isActive={activeTab === 2}
-          onClick={() => setActiveTab(2)}
-        />
-        <div className="flex px-8">
-          <div className="my-2 mx-2">
-                    <svg className="w-5 h-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                    </div>     
-        {/* Search bar */}
-        <input
-              type="text"
-              placeholder={
-                activeTab === 1
-                  ? 'Search by ID, name, or year & section...'
-                  : 'Search by name or role...'
-              }
-              value={filterText}
-              onChange={(event) => setFilterText(event.target.value)}
-              className="h-8 px-4 py-3 border border-gray-300 focus:ring-viridianHue focus:border-viridianHue  rounded-lg"
-            />
+          </button>
         </div>
-      </ul>
+      </div>
+        <ul className="flex mb-0 list-none flex-wrap px-4 flex-row text-gray-700">
+          <Tab
+            label="students list"
+            isActive={activeTab === 1}
+            onClick={() => setActiveTab(1)}
+          />
+          <Tab
+            label="employees list"
+            isActive={activeTab === 2}
+            onClick={() => setActiveTab(2)}
+          />
+          <div className="flex px-8">
+            <div className="my-2 mx-2">
+                <svg className="w-5 h-5 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>     
+            {/* Search bar */}
+            <input
+                type="text"
+                placeholder={
+                  activeTab === 1
+                    ? 'Search by ID, name, or year & section...'
+                    : 'Search by name or role...'
+                }
+                value={filterText}
+                onChange={(event) => setFilterText(event.target.value)}
+                className="h-8 px-4 py-3 border border-gray-300 focus:ring-viridianHue focus:border-viridianHue  rounded-lg"
+              />
+          </div>
+        </ul>
       <hr className="border-gray-300 mx-4" />
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
         <div className="px-4 py-5 flex-auto">
