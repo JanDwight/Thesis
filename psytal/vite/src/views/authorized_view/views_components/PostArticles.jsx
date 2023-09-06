668a7b06c7fbc1edd48ce4fb2091f95e24a8086d
Hilaria14 <lawaganhilaria@gmail.com>
2023-09-06 15:53:41 +0800

    update

--- psytal/vite/src/views/authorized_view/views_components/PostArticles.jsx
+++ psytal/vite/src/views/authorized_view/views_components/PostArticles.jsx
@@ -7,7 +7,6 @@ import announcement from "@assets/Announcement.jpg"
 import axiosClient from '../../../axios.js';
 import AddingPost from './AddingPost.jsx';
 import ArchivePost from './ArchivePost.jsx';
-import SampleArticle from './SampleArticle.jsx';
 
 
 export default function PostArticles() {
@@ -15,8 +14,31 @@ export default function PostArticles() {
     const [isShowMore, setIsShowMore] = useState(false);
     const toggleReadMoreLess = () => {
         setIsShowMore(!isShowMore);};
+    
+    {/**---------Calling the ArchivePost--------- */}
+    const [showArchivepost, setShowArchivepost] = useState(false);
+    const [errors, setErrors] = useState({ __html: '' });
+    const onSubmitarchive = async (ev) => {
+        ev.preventDefault();
+        setError({ __html: '' });
 
-    {/**---------Calling the Adding Post for it to edit hehe---------- */}
+        try {
+        const response = await axiosClient.post('/archivepost', { });
+        fetchLinks();
+        setShowLinks(false);
+        } catch (error) {
+        if (error.response) {
+            const finalErrors = Object.values(error.response.data.errors).reduce(
+            (accum, next) => [...accum, ...next],
+            []
+            );
+            setError({ __html: finalErrors.join('<br>') });
+        }
+        console.error(error);
+        }
+    };
+
+    {/**---------Calling the AddingPost for it to edit hehe---------- */}
     const [showPosts, setShowPosts]=useState(false);
     const [error, setError] = useState({ __html: '' });
     const [title, setTitle] = useState('');
@@ -45,9 +67,20 @@ export default function PostArticles() {
     <>
     {/*Post article */}
     <div class="px-10 my-4 py-6 bg-gray-200 rounded-2xl shadow-2xl">
-    
-        <SampleArticle>
-        <Menu className='placeholder:right-0 visible'>
+        <div class="flex justify-between items-center">
+            <div>
+                <a class="flex items-center" href="#">
+                    <img class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block" src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=373&q=80" alt="avatar"/>
+                    <div>
+                    <h1 class="text-gray-700 font-bold">Khatab wedaa</h1>
+                    <p class="font-light text-gray-600">mar 10, 2019</p>
+                    </div>
+                    
+                </a>
+                
+            </div>
+            
+            <Menu>
                 <Menu.Button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -66,7 +99,8 @@ export default function PostArticles() {
                             </Menu.Item>
                             <Menu.Item>
                                 {({ active }) => (
-                                    <a                                    
+                                    <a   
+                                        onClick={() => setShowArchivepost(true)}                                 
                                         className={`${active && 'bg-[#CCEFCC]'} border-b-2 border-black`}
                                         >
                                         Archive
@@ -79,8 +113,37 @@ export default function PostArticles() {
                 </Menu.Button>
                 
             </Menu>
-        </SampleArticle>
+            
+            
+        </div>
         
+        <div class="mt-2" onClick={toggleReadMoreLess}>
+            {/**Title of the post */}
+            <a class="text-2xl text-gray-700 font-bold hover:text-gray-600">Announcement: No Classess</a>
+            {/**Attached a photo here */}
+            <div className='flex justify-center items-center py-5'>
+                <img src={announcement} alt='announcement' height={150} width={150} ></img>
+            </div>
+            {/**BODY - Description of the post */}
+            <p className="mt-2" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Tempora expedita dicta totam aspernatur doloremque. 
+            Excepturi iste iusto eos enim reprehenderit nisi, 
+            accusamus delectus nihil quis facere in modi ratione libero!
+            {isShowMore && (
+                <span>
+                Sapiente exercitationem odio quia, animi eos distinctio tempora, ipsum
+                hic vitae modi eum nostrum id perspiciatis impedit dolores.Sapiente exercitationem odio quia, animi eos distinctio tempora, ipsum
+                hic vitae modi eum nostrum id perspiciatis impedit dolores.Sapiente exercitationem odio quia, animi eos distinctio tempora, ipsum
+                hic vitae modi eum nostrum id perspiciatis impedit dolores.Sapiente exercitationem odio quia, animi eos distinctio tempora, ipsum
+                hic vitae modi eum nostrum id perspiciatis impedit dolores.
+                </span>
+            )}
+            </p>
+            
+        </div>
+        <div class="flex justify-between items-center mt-4">
+            <a class="text-blue-600 hover:underline" onClick={toggleReadMoreLess}>{isShowMore ? "Read Less" : "Read More"}</a>
+        </div>
     </div>
 
     <AddingPost 
@@ -94,8 +157,11 @@ export default function PostArticles() {
     />
 
     <ArchivePost 
-    
+    showArchivepost={showArchivepost}
+    onclose={() => setShowArchivepost(false)}
+    onSubmitarchive={onSubmitarchive}
     />
     </>
   )
 }
+
