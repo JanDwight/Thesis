786d8fc594370297830e43c1bced4418d64cd0c2
HeraZhej <tuguinayjeborah@yahoo.com>
2023-09-21 22:27:00 +0800

    wafasf

--- psytal/app/Http/Controllers/PostController.php
+++ psytal/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\post;
 use Illuminate\Http\Request;
 
+
 class PostController extends Controller
 {
     /**
--- psytal/vite/src/views/authorized_view/views_components/AddingPost.jsx
+++ psytal/vite/src/views/authorized_view/views_components/AddingPost.jsx
@@ -1,76 +1,138 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import image from "@assets/icons8image.png";
-import announcement from "@assets/Announcement.jpg"
 
-export default function AddingPost({showPosts, title, setTitle, postmsg, setPostmsg, onClose, onSubmit}) {
-    if (!showPosts) {
-        return null;
-      }
+export default function AddingPost({ showPosts, onClose }) {
+  // Initialize state variables
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imagePreviews, setImagePreviews] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleImageChange = (event) => {
+    const files = event.target.files;
+    const previews = [];
+    const selected = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        previews.push(e.target.result);
+        selected.push(file);
+        if (previews.length === files.length) {
+          setImagePreviews(previews);
+          setSelectedImages(selected);
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      // Send the post data using axios (you need to implement this)
+      const postData = {
+        title,
+        description,
+        images: selectedImages,
+      };
+
+      // Example axios post request, replace with your actual API endpoint
+      await axios.post('/api/post', postData);
+
+      // Reset form fields and previews
+      setTitle('');
+      setDescription('');
+      setImagePreviews([]);
+      setSelectedImages([]);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error creating post:', error);
+      setLoading(false);
+    }
+  };
+
+  
+  if (!showPosts) {
+    return null;
+  }
+
   return (
     <>
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-        <div className="lg:w-1/2 min-w-[400px] max-w-[800px] px-4 py-1 shadow-lg h-fit bg-[#FFFFFF] rounded-3xl ring-1 ring-black mx-auto p-5">
-            <div className="w-full px-4 mx-auto mt-6"> 
-                <form onSubmit={onSubmit}>
-                    <div className="rounded-md bg-transparent p-3 w-30 h-30">
-                      <label htmlFor="upload" class="flex flex-row items-center gap-2 cursor-pointer">
-                        <img src={image} className="h-5 w-auto mt-5"/>
-                        <span className="text-md lg:text-md mx-2 font-semibold text-gray-500 mt-5">Attach Photo / File</span>
-                      </label>
-                      <input id="upload" type="file" class="hidden" />                    
-                    </div>
-                    
-                    <div>
-                        {/**TITLE */}
-                        <div className="flex items-center justify-between">
-                            <input 
-                            id="title"
-                            name="title"
-                            type="text"
-                            
-                            value={title}
-                            onChange={ev => setTitle(ev.target.value)}
-                            className="block w-1/2 rounded-3xl border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:leading-6 type=text" 
-                            placeholder="Title"
-                            />
-                        </div>
+      <div className="fixed top-0 left-0 w-full h-full overflow-y-auto bg-black bg-opacity-50">
+        <div className="lg:w-1/2 px-4 py-1 shadow-lg w-[20%] h-fit bg-[#FFFFFF] rounded-xl mt-[10%] mx-auto p-5">
+          <div className="w-full px-4 mx-auto mt-6">
+            <form onSubmit={handleSubmit}>
+              {/* Attach Photo / File */}
+              <div className="rounded-md bg-transparent p-3 w-30 h-30">
+                <label htmlFor="upload" className="flex flex-row items-center gap-2 cursor-pointer">
+                  <img src={image} className="h-8 w-auto mt-5" alt="Upload Icon" />
+                  <span className="text-md lg:text-md mx-2 font-semibold text-green-800 mt-5">Attach Photo / File</span>
+                </label>
+                <input id="upload" type="file" className="hidden" multiple onChange={handleImageChange} />
+              </div>
 
-                    </div>
-                    
-                    {/**Attached a photo here */}
-                    <div className='flex justify-center items-center py-5'>
-                        <img src={announcement} alt='announcement' height={150} width={150} ></img>
-                    </div>
+              {/* Title Input */}
+              <div className="flex items-center justify-between mt-4">
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  value={title}
+                  onChange={(ev) => setTitle(ev.target.value)}
+                  className="block w-1/2 rounded-xl border-1 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:leading-6"
+                  placeholder="Title"
+                />
+              </div>
 
-                    {/**POST */}
-                    <div className="mt-2">
-                    <textarea
-                        id="postmsg"
-                        name="postmsg"
-                        
-                        value={postmsg}
-                        onChange={ev => setPostmsg(ev.target.value)}
-                        className="block w-full rounded-3xl border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:leading-6"
-                        rows="4" // Adjust the number of rows as needed
-                        placeholder="Post about..."
-                    />
+              {/* Image Previews */}
+              {imagePreviews.length > 0 && (
+                <div className="mt-4 grid grid-cols-2 gap-2">
+                  {imagePreviews.map((preview, index) => (
+                    <div key={index} className="max-w-full">
+                      <img src={preview} alt={`Preview ${index}`} className="max-w-full h-auto rounded-lg" />
                     </div>
+                  ))}
+                </div>
+              )}
 
-                    {/**BUTTONS */}
-                    <div className="text-center flex justify-end my-7">
-                        <button onClick={onClose} className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 mr-6 rounded-full">
-                            Cancel
-                        </button>
-                        <button type="submit" className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full">
-                            Post
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>     
-    </div>
-    
+              {/* Description Textarea */}
+              <div className="mt-4">
+                <textarea
+                  id="description"
+                  name="description"
+                  value={description}
+                  onChange={(ev) => setDescription(ev.target.value)}
+                  className="block w-full rounded-xl border-1 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:leading-6"
+                  rows="4"
+                  placeholder="Write post ..."
+                />
+              </div>
+
+              {/* Buttons */}
+              <div className="text-center flex justify-end my-7">
+                <button onClick={onClose} className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 mr-6 rounded-full">
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full"
+                  disabled={loading}
+                >
+                  {loading ? 'Posting...' : 'Post'}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
