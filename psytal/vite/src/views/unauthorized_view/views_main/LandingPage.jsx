2eef81a731b28fae98f3bc45c58db7036af3408d
JVernonDV <jeoffdevera182@gmail.com>
2023-09-25 17:15:50 +0800

    Admin and Landing Page Footer

--- psytal/vite/src/views/unauthorized_view/views_main/LandingPage.jsx
+++ psytal/vite/src/views/unauthorized_view/views_main/LandingPage.jsx
@@ -191,27 +191,36 @@ export default function LandingPage() {
    
 
         {/**<!--Footer--> */}
-        <footer className='bg-neutral-100 text-center text-gray-500 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left'>
-              {/**<!-- Social icons --> */}
-              <div className='border-b-2 border-neutral-200 p-6 dark:border-neutral-500'>
-                <div className='container mx-auto px-10'>
-                  <div className='flex items-center justify-center lg:justify-between'>
-                    <div className='mr-12 hidden lg:block'>
-                      <span>Get connected with us on social networks: BSU-CSS-Department of Psychology</span>
-                    </div>
-                    <div className='flex justify-center'>
-                      {/**FB */}
-                      <a href="#!" className="mr-6 text-gray-500 dark:text-gray-200">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            class="h-4 w-4"
-                            fill="currentColor"
-                            viewBox="0 0 24 24">
-                          <path d=""/>
-                        </svg>
-                      </a>
-             
+        <footer className='bg-neutral-100 text-center text-gray-500 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left mt-[1%]'>
+      {/**<!-- Social icons --> */}
+      <div className='border-b-2 border-neutral-200 p-3 dark:border-neutral-500'>
+        <div className='container mx-auto px-10'>
+          <div className='flex items-center justify-center lg:justify-between'>
+            <div className='mr-12 hidden lg:block'>
+              <span>Get connected with us on social networks: BSU-CSS-Department of Psychology</span>
+            </div>
+            <div className='flex justify-center'>
+              {/**FB */}
+              <a href="https://www.facebook.com/psychologybsu" target="_blank" className="mr-6 text-gray-500 dark:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="currentColor"
+                    viewBox="0 0 24 24">
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
+                </svg>
+              </a>
+              {/**Twitter */}
+              <a href="#!" target="_blank" className="mr-6 text-gray-500 dark:text-gray-200">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="currentColor"
+                    viewBox="0 0 24 24">
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                </svg>
+              </a>
 
-              <a a href="https://www.facebook.com/psychologybsu" className="mr-6 text-gray-500 dark:text-gray-200">
+                  {/**Instagram */}
+              <a href="https://www.instagram.com/benguetstateuniversityofficial/" target="_blank" className="mr-6 text-gray-500 dark:text-gray-200">
                 <svg xmlns="http://www.w3.org/2000/svg"
                     class="h-5 w-5"
                     fill="currentColor"
@@ -222,7 +231,8 @@ export default function LandingPage() {
             </div>
           </div>
         </div>
-      </div> {/**End of Social Icons */}
+      </div> 
+         {/**End of Social Icons */}
 
       {/*Footer*/}
       <div className='container w-full px-10'>
@@ -246,20 +256,21 @@ export default function LandingPage() {
             {/**---------------COL3------------- */}
             <div className='mb-4 mx-10 flex flex-col justify-center font-semibold uppercase md:justify-start'>
               <h6 >
-                Useful Links
+                <a href="https://www.google.com/maps/@16.4529815,120.5897683,3a,75y,62.24h,81.21t/data=!3m6!1e1!3m4!1s3g6UBbP1ms4hzPLFCynOug!2e0!7i16384!8i8192?entry=ttu" target="_blank">
+                Site Map
+                </a>
               </h6>
               <p>
-                <a href="http://www.bsu.edu.ph/" class=" text-black-200">need Help?</a>
+                <a href="http://www.bsu.edu.ph/" target="_blank" class=" text-black-200">Benguet state university website</a>
               </p>
             </div>
 
             {/**--------------COL4------------- */}
-            <div className=' flex flex-col font-semibold uppercase'>
-              <h6 >
-                Contact Us
+            <div>
+              <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                Contact
               </h6>
-              <div className=''>
-              <p >
+              <p className='mb-4 flex items-center justify-center md:justify-start'>
                 <svg xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
@@ -267,10 +278,11 @@ export default function LandingPage() {
                   <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"/>
                   <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"/>
                 </svg>
+                <a href="https://www.google.com/maps/dir//Benguet+State+University,+La+Trinidad,+Benguet/@16.4544374,120.5894334,528m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x3391a3bb4279be9b:0xbf6e126a84be4efc!2m2!1d120.5902746!2d16.4543609?entry=ttu" target="_blank" class=" text-black-200">
                 Km.5 La Trinidad, Benguet
+                </a>
                 </p>
-                </div>
-                <p >
+                <p className='mb-4 flex items-center justify-center md:justify-start'>
                   <svg xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
@@ -280,7 +292,7 @@ export default function LandingPage() {
                   </svg>
                   psychology@gmail.com
                 </p>
-                <p >
+                <p className='mb-4 flex items-center justify-center md:justify-start'>
                   <svg xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
@@ -289,12 +301,13 @@ export default function LandingPage() {
                       d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
                       clip-rule="evenodd"/>
                   </svg>
-                  +63 9123 456 789
+                  +639123456732
                 </p>
             </div>
           </div>
         </div>
-      </div> {/** End of the Main Footer Content */}
+      </div> 
+      {/** End of the Main Footer Content */}
 
       {/**<!-- Copyrights --> */}
       <div className=' p-6 text-center bg-neutral-700'>
