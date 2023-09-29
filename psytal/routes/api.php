1d218f3ac51962b60e2f50b0c428f603448384b8
JVernonDV <jeoffdevera182@gmail.com>
2023-09-30 03:39:42 +0800

    Linksupdate(2)

--- psytal/routes/api.php
+++ psytal/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CurriculumController;
+use App\Http\Controllers\LinksController;
 use App\Http\Controllers\PreregistrationIncomingTmpController;
 use App\Http\Controllers\TmpPreregistrationController;
 use App\Http\Controllers\PostController;
@@ -13,7 +14,7 @@ use App\Http\Controllers\UpdateClassesController;
 use App\Http\Controllers\ArchiveClassesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LinksController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -47,7 +48,7 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::post('/addcurriculum', [CurriculumController::class, 'addCurriculum']);
 });
 
-Route::post('/addlink', [LinksController::class, 'store']);
+Route::post('/addlink', [LinksController::class, 'addLink']);
 Route::get('/getlinks', [LinksController::class, 'getLinks']);
 Route::post('/archivelink', [LinksController::class, 'archiveLink']);
 
