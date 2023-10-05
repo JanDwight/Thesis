86c1bc78d4570aaa4f0935e8a7d9d6010818fdc9
jjhonalyn <127106297+jjhonalyn@users.noreply.github.com>
2023-10-06 01:21:26 +0800

    curriculum-edit

--- psytal/routes/api.php
+++ psytal/routes/api.php
@@ -63,4 +63,3 @@ Route::post('/preregincommingtmp', [PreregistrationIncomingTmpController::class,
 
 Route::get('/getcurriculum', [CurriculumController::class, 'getCurriculum']);
 Route::put('/archivecurriculum/{id}', [CurriculumController::class, 'archiveCurriculum']);
-Route::post('/archivecurriculum', [CurriculumController::class, 'archiveCurriculum']);
