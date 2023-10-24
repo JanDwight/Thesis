3e0de661483e63ff7eb567c5aafab3b90b566665
JVernonDV <jeoffdevera182@gmail.com>
2023-10-25 06:31:06 +0800

    Admin and Landing Page Footer

--- psytal/vite/src/views/authorized_view/views_main/AdminLayout.jsx
+++ psytal/vite/src/views/authorized_view/views_main/AdminLayout.jsx
@@ -300,7 +300,7 @@ export default function AdminLayout() {
       </ReactModal>
 
       {/**<!--Footer--> */}
-      <footer className='bg-neutral-100 text-center text-gray-500 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left mt-[1%]'>
+      <footer className='bg-neutral-200 text-center text-gray-500 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left mt-[1%]'>
       {/**<!-- Social icons --> */}
       <div className='border-b-2 border-neutral-200 p-3 dark:border-neutral-500'>
         <div className='container mx-auto px-10'>
@@ -344,66 +344,64 @@ export default function AdminLayout() {
          {/**End of Social Icons */}
 
       {/**<!-- Main footer content --> */}
-      <div className='container mx-auto px-4'>
-        <div className='mx-6 py-3 text-center md:text-left'>
-          <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
-            {/**----------------COL1------------- */}
-            <div>
-              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
-                <svg xmlns="http://www.w3.org/2000/svg"
+        <div className="bg-gray-300">
+        <div className='container mx-auto px-4 py-5 justify-center items-center h-full'>
+          <div className='mx-6 text-center'>
+            <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-5'>
+              <div></div>
+              {/**----------------COL1------------- */}
+              <div>
+                <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+                  <svg xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
                     className="mr-3 h-4 w-4">
-                  <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z"/>
-                </svg>
-                Psychology Department
-              </h6>
-              <p>REPUBLIC OF THE PHILIPPINES </p>
-              <p>All content is in the public domain unless otherwise stated.</p>
-            </div>
-            {/**----------------COL2------------- */}
-            <div>
-              <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
-                About Psychology Department
-              </h6>
-              <p className='mb-4'>
-                <a href="#!" className="text-gray-500 dark:text-neutral-200">Misson</a>
-              </p>
-              <p className='mb-4'>
-                <a href="#!" className="text-gray-500 dark:text-neutral-200">Visson</a>
-              </p>
-              <p className='mb-4'>
-                <a href="#!" className="text-gray-500 dark:text-neutral-200">Goals</a>
-              </p>
-            </div>
-            {/**---------------COL3------------- */}
-            <div className='mb-4 mx-10 flex flex-col justify-center font-semibold uppercase md:justify-start'>
-              <h6 >
-                <a href="https://www.google.com/maps/@16.4529815,120.5897683,3a,75y,62.24h,81.21t/data=!3m6!1e1!3m4!1s3g6UBbP1ms4hzPLFCynOug!2e0!7i16384!8i8192?entry=ttu" target="_blank">
-                Site Map
-                </a>
-              </h6>
-              <p>
-                <a href="http://www.bsu.edu.ph/" target="_blank" className=" text-black-200">BSU website</a>
-              </p>
-            </div>
-
-            {/**--------------COL4------------- */}
-            <div>
-              <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
-                Contact
-              </h6>
-              <p className='mb-4 flex items-center justify-center md:justify-start'>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="mr-3 h-5 w-5">
-                  <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"/>
-                  <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"/>
-                </svg>
-                <a href="https://www.google.com/maps/dir//Benguet+State+University,+La+Trinidad,+Benguet/@16.4544374,120.5894334,528m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x3391a3bb4279be9b:0xbf6e126a84be4efc!2m2!1d120.5902746!2d16.4543609?entry=ttu" target="_blank" className=" text-black-200">
-                Km.5 La Trinidad, Benguet
-                </a>
+                    <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z"/>
+                  </svg>
+                  Psychology Department
+                </h6>
+                <p>REPUBLIC OF THE PHILIPPINES</p>
+                <p>All content is in the public domain unless otherwise stated.</p>
+              </div>
+          
+              {/**---------------COL2------------- */}
+              <div className='mb-4 flex flex-col justify-center font-semibold uppercase md:justify-start'>
+                <h6>
+                  <a href="https://www.google.com/maps/@16.4529815,120.5897683,3a,75y,62.24h,81.21t/data=!3m6!1e1!3m4!1s3g6UBbP1ms4hzPLFCynOug!2e0!7i16384!8i8192?entry=ttu" target="_blank">
+                  Site Map
+                  </a>
+                </h6>
+                <p>
+                  <a href="http://www.bsu.edu.ph/" target="_blank" className=" text-black-200">BSU website</a>
+                </p>
+              </div>
+              {/**--------------COL3------------- */}
+              <div>
+                <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                  Contact
+                </h6>
+                <p className='mb-4 flex items-center justify-center md:justify-start'>
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="mr-3 h-5 w-5">
+                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"/>
+                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"/>
+                  </svg>
+                  <a href="https://www.google.com/maps/dir//Benguet+State+University,+La+Trinidad,+Benguet/@16.4544374,120.5894334,528m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x3391a3bb4279be9b:0xbf6e126a84be4efc!2m2!1d120.5902746!2d16.4543609?entry=ttu" target="_blank" className=" text-black-200">
+                  Km.5 La Trinidad, Benguet
+                  </a>
+                </p>
+                <p className='mb-4 flex items-center justify-center md:justify-start'>
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="mr-3 h-5 w-5">
+                    <path fillRule="evenodd"
+                      d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.970c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.970-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                      clipRule="evenodd"/>
+                  </svg>
+                  psychology@gmail.com
                 </p>
                 <p className='mb-4 flex items-center justify-center md:justify-start'>
                   <svg xmlns="http://www.w3.org/2000/svg"
@@ -413,27 +411,17 @@ export default function AdminLayout() {
                     <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z"/>
                     <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z"/>
                   </svg>
-                  psychology@gmail.com
+                  +639123456732
                 </p>
-                <p className='mb-4 flex items-center justify-center md:justify-start'>
-                  <svg xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="mr-3 h-5 w-5">
-                    <path fillRule="evenodd"
-                      d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                      clipRule="evenodd"/>
-                  </svg>
-                  +63 9000 0000 000
-                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div> 
+      </div>
       {/** End of the Main Footer Content */}
 
       {/**<!-- Copyrights --> */}
-      <div className='bg-neutral-200 p-3 text-center dark:bg-neutral-700'>
+      <div className='bg-gray-200 p-3 text-center dark:bg-neutral-700'>
         <span>© 2023 Copyright:</span>
         <a font-semibold="true" text-neutral-600="true" dark:text-neutral-400="true">Psychology Department</a>
       </div> {/**End of copyrights */}
