26350ccc745a29bf620e4a7a5c6b6a097e8fb0e2
JVernonDV <jeoffdevera182@gmail.com>
2023-12-08 23:29:24 +0800

    Prompts Again- for forgot password [JB]

--- psytal/vite/src/views/unauthorized_view/views_components/Forgot_Password/InputCode.jsx
+++ psytal/vite/src/views/unauthorized_view/views_components/Forgot_Password/InputCode.jsx
@@ -1,24 +1,20 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import axiosClient from '../../../../axios';
 
-export default function InputCode() {
+export default function InputCode({ closeModal, propData}) {
   const [userCode, setUserCode] = useState('');
-  const location = useLocation();
-  const formData = location.state ? location.state.formData : null;
   const [newPassword, setNewPassword] = useState('');
+  const [successMessage, setSuccessMessage] = useState(null);
 
   const onSubmit = async (ev) => {
     ev.preventDefault();
 
     // Check if userCode matches the code in formData
-    if (formData && userCode === formData.code) {
+    if (propData && userCode === propData.code) {
       // Code is correct, you can proceed with your logic
-      console.log(formData);
-
       try {
         const response = await axiosClient.put('/changepassword', {
-          email: formData.email,
+          email: propData.email,
         });
 
         // Assuming the API response structure is { new_password: 'your_password' }
@@ -29,30 +25,39 @@ export default function InputCode() {
         // Preparing formData to be sent to the backend
         const newFormData = new FormData();
         newFormData.append('newPassword', newPasswordFromResponse);
-        newFormData.append('email', formData.email);
+        newFormData.append('email', propData.email);
 
         await axiosClient.get('/sendnewpassword', {
           params: Object.fromEntries(newFormData), // Convert FormData to plain object
         });
 
-        console.log(response.data); // You can handle the response as needed
+        setSuccessMessage({
+          message: 'Your password was changed successfully!\n Please check your email for your new password.',
+        });
+
+        setTimeout(() => {
+          setSuccessMessage(null);
+          closeModal();
+          window.location.reload();
+        }, 7000);
+
       } catch (error) {
         console.error('Error updating password:', error);
         // Handle error appropriately
       }
     } else {
       // Code is incorrect, handle accordingly
-      console.log('Code is incorrect');
+      console.log("This is the code" + propData.code);
       // Add your logic for incorrect code
     }
   };
 
   return (
     <>
-      <div className='flex min-h-full flex-1 flex-col items-center justify-center px-6 py-12 lg:px-8 bg-gradient-to-r from-green-800 via-green-500 to-green-800'>
+      <div className='flex min-h-full flex-1 flex-col items-center justify-center px-6 py-12 lg:px-8'>
         <div className='flex items-center justify-between'>
-          <label htmlFor='password' className='block text-sm font-medium leading-6 text-gray-900'>
-            Please Enter The Code We Sent You
+          <label htmlFor='password' className='block text-xl font-medium leading-6 text-gray-900'>
+            Please Enter The 4-Digit Code We Sent You:
           </label>
         </div>
 
@@ -61,7 +66,7 @@ export default function InputCode() {
             <input
               name='end'
               type='text'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 '
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
               value={userCode}
               onChange={(ev) => setUserCode(ev.target.value)}
             />
@@ -77,6 +82,17 @@ export default function InputCode() {
           </div>
         </form>
       </div>
+      {successMessage && (
+        <div className="fixed top-0 left-0 w-full h-full overflow-y-auto bg-black bg-opacity-50">
+          <div className="lg:w-1/2 px-4 py-1 shadow-lg w-[20%] h-fit bg-[#FFFFFF] rounded-xl mt-[10%] mx-auto p-5">
+            <div className="w-full px-4 mx-auto mt-6">
+              <div className="text-center text-xl text-green-600 font-semibold my-3">
+                {successMessage.message}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
