eb3754da973de67e7ad3b14753806bb2f7bded5d
Hilaria14 <lawaganhilaria@gmail.com>
2024-01-12 16:25:21 +0800

    for viewing of phone

--- psytal/vite/src/views/authorized_view/views_components/AddingPost.jsx
+++ psytal/vite/src/views/authorized_view/views_components/AddingPost.jsx
@@ -121,7 +121,7 @@ export default function AddingPost() {
 
       {isModalOpen && (
         <div className="fixed top-0 left-0 w-full h-full overflow-y-auto bg-black bg-opacity-50">
-          <div className="lg:w-1/2 px-4 py-1 shadow-lg w-[20%] h-fit bg-[#FFFFFF] rounded-xl mt-[10%] mx-auto p-5">
+          <div className="w-full px-4 py-1 shadow-lg sm:w-[70%] h-fit bg-[#FFFFFF] rounded-xl mt-[10%] mx-auto p-5">
             <div className="w-full px-4 mx-auto mt-6">
               <form onSubmit={onSubmit}>
                 {/* Attach Photo / File */}
