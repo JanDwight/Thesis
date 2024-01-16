2e104ca697b9901e27569d293455f4c93ae2577a
Hilaria14 <lawaganhilaria@gmail.com>
2024-01-16 23:01:51 +0800

    fix the student mobile menu button

--- psytal/vite/src/views/authorized_view/views_main/DefaultLayout.jsx
+++ psytal/vite/src/views/authorized_view/views_main/DefaultLayout.jsx
@@ -167,7 +167,7 @@
                               isActive
                                 ? 'bg-[#3A4D39] text-white'
                                 : 'text-gray-300 hover:bg-gray-500 hover:text-white',
-                              'rounded-md px-3 py-2 text-l font-medium'
+                              'block rounded-md px-3 py-2 text-l font-medium'
                             )
                           }
                         >
